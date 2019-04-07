24595d06fcbfdb38813b9e36d6ea8b0d9d8c0397
Radovan <studio@pythonforce.com>
2019-04-07 15:04:02 -0400

    debug false

--- config/settings/common.py
+++ config/settings/common.py
@@ -73,7 +73,7 @@ MIDDLEWARE = [
 # DEBUG
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = env.bool("DJANGO_DEBUG", True)
+DEBUG = env.bool("DJANGO_DEBUG", False)
 
 # URL Configuration
 # ------------------------------------------------------------------------------
